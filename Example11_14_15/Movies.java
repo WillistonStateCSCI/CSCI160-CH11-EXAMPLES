6bbdc5b11b79bfb5e4eb05cc3226427a7ac4624e
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2026-02-26 16:45:23 +0000

    updated movies to run in codespaces

--- Example11_14_15/Movies.java
+++ Example11_14_15/Movies.java
@@ -3,72 +3,56 @@
 */
 
 import java.io.*;
-import java.net.*;
 import java.util.*;
 import org.json.*;
 
-public class Movies
-{
-  public static void main( String args [ ] ) 
-  {
-    String GITHUB = "https://raw.githubusercontent.com/";
-	 String fileName = GITHUB +
-      "prust/wikipedia-movie-data/master/movies.json";
-    
-	 try 
-    {
-      URL url = new URL( fileName );
-      InputStream is = url.openStream( );
-      Scanner file = new Scanner( is );
+public class Movies {
+  public static void main(String args[]) {
+    String fileName = "Example11_14_15/movies.json";
+
+    try {
+      File fileObj = new File(fileName);
+      Scanner file = new Scanner(fileObj);
       String json = "";
       String s = null;
-      while ( file.hasNext( ) )
-      {
-        s = file.nextLine( );
+      while (file.hasNext()) {
+        s = file.nextLine();
         json += s;
       }
-      
-      JSONArray jsonArray = new JSONArray( json );
-      System.out.println( "There are " + jsonArray.length( ) 
-                                       + " movies" );
-                                 
+      file.close();
+
+      JSONArray jsonArray = new JSONArray(json);
+      System.out.println("There are " + jsonArray.length()
+          + " movies");
+
       // Build ArrayList of MovieRecord objects
-      ArrayList<Movie> movies = new ArrayList<Movie>( );
-      
-      for ( int i = 0; i < jsonArray.length( ); i++ )
-      {
-        JSONObject jsonMovie = jsonArray.getJSONObject( i ); 
-        String title = jsonMovie.getString( "title" );
-        int year = jsonMovie.getInt( "year" );
-        if ( title.equals( "" ) )
-          System.out.println( "Discarding (no title)" );
+      ArrayList<Movie> movies = new ArrayList<Movie>();
+
+      for (int i = 0; i < jsonArray.length(); i++) {
+        JSONObject jsonMovie = jsonArray.getJSONObject(i);
+        String title = jsonMovie.getString("title");
+        int year = jsonMovie.getInt("year");
+        if (title.equals(""))
+          System.out.println("Discarding (no title)");
         else
-          movies.add( new Movie( title, year ) );
-      }                               
-                
+          movies.add(new Movie(title, year));
+      }
+
       // Ask user to enter a year
-      Scanner scan = new Scanner( System.in );
-      System.out.print( "Please enter a year > " );
-      int year = scan.nextInt( );
-      
+      Scanner scan = new Scanner(System.in);
+      System.out.print("Please enter a year > ");
+      int year = scan.nextInt();
+      scan.close();
       // Retrieve number of movies for that year
-      long count = movies.stream( )
-                         .filter( movie -> movie.year( ) == year )
-                         .count( );
-      System.out.println( "There were " + count 
-                          + " movies in " + year );
-    } 
-    catch( MalformedURLException murle ) 
-    {
-		System.out.println( murle.getMessage( ) );
-	 }
-    catch( IOException ioe ) 
-    {
-		System.out.println( ioe.getMessage( ) );
-	 }
-    catch( JSONException jsone )
-    {
-      System.out.println( jsone.getMessage( ) );
+      long count = movies.stream()
+          .filter(movie -> movie.year() == year)
+          .count();
+      System.out.println("There were " + count
+          + " movies in " + year);
+    } catch (IOException ioe) {
+      System.out.println(ioe.getMessage());
+    } catch (JSONException jsone) {
+      System.out.println(jsone.getMessage());
     }
   }
 }
